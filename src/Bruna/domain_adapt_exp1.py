1e98e4b735f05d6e76b03e058367470773423fd1
brunalopes <brunaafl@usp.br>
2024-08-20 07:14:41 -0300

    Adding results 0.0001 lr

--- src/Bruna/domain_adapt_exp1.py
+++ src/Bruna/domain_adapt_exp1.py
@@ -155,7 +155,7 @@ def main(args):
     # Save results
     print(run_dir)
     print(experiment_name)
-    results.to_csv(f"{run_dir}/Test-Heads-{experiment_name}_{args.remove_bn}_{eval_config.train.lr}_{args.mode}_results.csv")
+    results.to_csv(f"{run_dir}/Heads-{experiment_name}_{args.remove_bn}_{eval_config.train.lr}_{args.mode}_results.csv")
     
     print("---------------------------------------")
 
