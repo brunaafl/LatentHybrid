29afcecfaf0285beea11655588970defe9f8fe34
brunalopes <brunaafl@usp.br>
2025-02-07 09:24:53 +0100

    Adding results with 5, 10 and 15 center loss

--- src/Bruna/alignment_loss.py
+++ src/Bruna/alignment_loss.py
@@ -6,7 +6,7 @@ from numpy import random
 
 
 class JointAlignmentLoss(nn.Module):
-    def __init__(self, feat_dim=(16, 1, 251), num_classes=2, centroids=None, lambd = 1):
+    def __init__(self, feat_dim=(16, 1, 251), num_classes=2, centroids=None, lambd = 5):
         super(JointAlignmentLoss, self).__init__()
 
         self.feat_dim=feat_dim
--- src/Bruna/domain_adapt_exp1.py
+++ src/Bruna/domain_adapt_exp1.py
@@ -164,8 +164,8 @@ def main(args):
     # Save results
     print(run_dir)
     print(experiment_name)
-    print(f"{run_dir}/2-centers_1_{experiment_name}_{args.remove_bn}_{eval_config.train.lr}_{args.mode}_results.csv")
-    results.to_csv(f"{run_dir}/2-centers_1_{experiment_name}_{args.remove_bn}_{eval_config.train.lr}_{args.mode}_results.csv")
+    print(f"{run_dir}/2-centers_lamb5_{experiment_name}_{args.remove_bn}_{eval_config.train.lr}_{args.mode}_results.csv")
+    results.to_csv(f"{run_dir}/2-centers_lamb5_{experiment_name}_{args.remove_bn}_{eval_config.train.lr}_{args.mode}_results.csv")
 
     print("---------------------------------------")
 
