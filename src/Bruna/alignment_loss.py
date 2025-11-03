f48feaff2adb1a373cd80e5c83840a8d450dbdaa
brunalopes <brunaafl@usp.br>
2025-11-03 15:14:43 +0100

    Adjusting center alignment MSE

--- src/Bruna/alignment_loss.py
+++ src/Bruna/alignment_loss.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 from numpy import random
 
 class JointAlignmentLoss(nn.Module):
-    def __init__(self, feat_dim=(16, 1, 501), num_classes=2, centroids=None, lambd = 10):
+    def __init__(self, feat_dim=(16, 1, 501), num_classes=2, centroids=None, lambd = 1):
         super(JointAlignmentLoss, self).__init__()
 
         self.feat_dim=feat_dim
